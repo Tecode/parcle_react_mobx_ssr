e5d1b6859ea109d0ea16c0b58a0f2a619fd5a0ef
谢明 <ming.xie@socialcredits.cn>
2018-06-05 18:14:20 +0800

    feat: 完善ant组件

--- app/containers/NavBar/index.jsx
+++ app/containers/NavBar/index.jsx
@@ -4,6 +4,7 @@ import Layout from 'antd/lib/layout'
 import Menu from 'antd/lib/menu'
 import Icon from 'antd/lib/icon'
 import 'antd/lib/layout/style';
+import styles from './index.scss';
 const { Header, Sider, Content } = Layout;
 
 export default class NavBar extends Component {
@@ -20,17 +21,18 @@ export default class NavBar extends Component {
   }
   render() {
     return (
-      <Layout>
+      <Layout className={styles.layout}>
         <Sider
           trigger={null}
           collapsible
+          style={{ background: '#fff' }}
           collapsed={this.state.collapsed}
         >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <div className={styles.logo} >Logo</div>
+          <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} style={{height: '100%'}}>
             <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
+              <Icon type="form" />
+              <span>新增文章</span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="video-camera" />
@@ -45,7 +47,7 @@ export default class NavBar extends Component {
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
             <Icon
-              className="trigger"
+              className={styles.trigger}
               type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
